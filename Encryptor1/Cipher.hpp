d9b953f314e5576e24b58e9620894e347c8b0838
Jefferson798 <reyc20962@gmail.com>
2025-01-02 13:23:02 -0800

    Added to cipher

--- Encryptor1/Cipher.hpp
+++ Encryptor1/Cipher.hpp
@@ -1,2 +1,19 @@
 #pragma once
 //this is a cipher 
+
+#include <iostream> 
+#include <string> 
+
+using std::cin; 
+using std::cout; 
+using std::endl; 
+
+using std::string; 
+
+class Cipher
+{
+public:
+	virtual string encode(string& msg) = 0; 
+	virtual string decode(string& msg) = 0;
+private:
+};
--- Encryptor1/Main.cpp
+++ Encryptor1/Main.cpp
@@ -6,6 +6,7 @@ int main()
 
 	std::cout << "Welcome to the new era!" << std::endl;
 	//We will need a class that will take care of the menu
+
 	return 0;
 
 
